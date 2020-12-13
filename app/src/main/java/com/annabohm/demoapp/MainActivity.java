92a06ef21889d51a9c41cde14577d113f3ba518b
Ania <ania.bohm@gmail.com>
2020-12-13 15:36:29 +0100

    faulty upload

--- app/src/main/java/com/annabohm/demoapp/MainActivity.java
+++ app/src/main/java/com/annabohm/demoapp/MainActivity.java
@@ -8,12 +8,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
-    NavController navController;
+    NavController navController = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
 
