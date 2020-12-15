1de9822f4d8808cf363c1a55764bd6d09d28d3e7
Ludwik Rydzak <ludwik.rydzak@gmail.com>
2020-12-15 20:50:43 +0100

    Expanding photo added

--- app/src/main/java/com/annabohm/demoapp/Adapter.java
+++ app/src/main/java/com/annabohm/demoapp/Adapter.java
@@ -1,8 +1,11 @@
 package com.annabohm.demoapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +14,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 
@@ -30,6 +35,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     LayoutInflater inflater;
     DatabaseReference mReference;
     Context context;
+    NavController navController;
 
     public Adapter(Context context, List<String> photos) {
         this.photos = photos;
@@ -41,6 +47,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = inflater.inflate(R.layout.mini_photo, parent, false);
+
         return new ViewHolder(view);
     }
 
@@ -48,7 +55,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
 
         Picasso.get().load(photos.get(position)).resize(400,400).centerCrop().into(holder.photo);
-        //holder.photo.setImageResource(photos.get(position));
+//        Picasso.get().load(photos.get(position)).resize(400,400).centerCrop().into(holder.bigPhoto);
     }
 
     @Override
@@ -59,16 +66,25 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         ImageView photo;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             photo = itemView.findViewById(R.id.photo);
+
             photo.setOnClickListener(this);
+
         }
 
         @Override
         public void onClick(final View view) {
+            Bundle bundle = new Bundle();
             final String[] toast_message = new String[1];
             final int position = this.getLayoutPosition();
+            navController = Navigation.findNavController(view);
+            BitmapDrawable drawable = (BitmapDrawable) photo.getDrawable();
+            Bitmap bitmap = drawable. getBitmap();
+            bundle.putParcelable("photo",bitmap);
+            navController.navigate(R.id.galleryToPhoto, bundle);
         }
     }
 }
